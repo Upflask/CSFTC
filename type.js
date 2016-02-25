a207c1053db80389fac4b2e47428f09fb69fbc98
Bitstick <thebitstick@tfwno.gf>
2016-02-25 10:36:04 -0600

    Wrong file

--- type.js
+++ type.js
@@ -1,6 +1,11 @@
-  document.getElementById("file").onchange = function() {
-    document.getElementById("uploadText").innerHTML = "Uploading...";
-    var button = document.getElementById("uploadButton");
-    button.className = button.className + " uploading";
-    document.getElementById("form").submit();
-  };
+$(document).ready(function() {
+   $("#file").change(function () 
+   { 
+     var ext = $('#file').val().split('.').pop().toLowerCase();
+     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass']) == -1)
+      {
+    	alert('This file extension is not allowed, please check the terms.');
+        location.reload();
+} 
+  }); 
+});
