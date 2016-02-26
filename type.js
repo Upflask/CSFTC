b2f961b1eeae80aaee14d944c3cf0dede22c5120
Bitstick <thebitstick@tfwno.gf>
2016-02-26 10:09:40 -0600

    Added .flac, .psd, and .mkv

--- type.js
+++ type.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    $("#file").change(function () 
    { 
      var ext = $('#file').val().split('.').pop().toLowerCase();
-     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass']) == -1)
+     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass','mkv','psd','flac']) == -1)
       {
     	alert('This file extension is not allowed, please check the terms.');
         location.reload();
